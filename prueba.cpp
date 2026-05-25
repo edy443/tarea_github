ec9029d1f4df34c9c0a30371b1f02c6e99e884c1
eduardo <gabrielcelada50@gmail.com>
2026-05-25 16:31:39 -0600

    Update prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -8,12 +8,21 @@ cout << "hola este es un programa de prueba "<<endl;
 
 cout << "esta es la version 1. "<<endl;
 
-
+int num;
 
     cout << "esta parte la edite desde github.com "<<endl; 
 //PRUEBA
 
 cout <<"esa es la version 2" <<endl;
+
+
+cout << "Ingrese un numero: ";
+    cin >> num;
+
+    // El bucle 'for' inicializa i en 1, evalúa que no pase de 'num' y aumenta de 1 en 1
+    for (int i = 1; i <= num; i++) 
+    {
+        cout << i << " veces" << endl;
 
 
     return 0;
