0e4c27a8ead2d6a085d07bb7eca7f8980aa24091
eduardo <gabrielcelada50@gmail.com>
2026-05-25 16:03:43 -0600

    Create prueba.cpp

---
+++ prueba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+cout << "hola este es un programa de prueba "<<endl;
+
+
+cout << "hola este es un programa de prueba "<<endl;
+
+
+
+    return 0;
+}
